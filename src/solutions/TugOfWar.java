b5e20cb0986a8310e8c4dceb11bdee61754fdf8e
Матей Штясек <85434181+matthew-shtyasek@users.noreply.github.com>
2021-11-12 22:53:40 +0500

    Update TugOfWar.java

--- src/solutions/TugOfWar.java
+++ src/solutions/TugOfWar.java
@@ -1,16 +1,8 @@
 package solutions;
 
 public class TugOfWar {
-    private int countLeft = 0,
-                countRight = 0;
 
-    public void addLeft(int countLeft) { this.countLeft = countLeft; }
-    public void addRight(int countRight) {this.countRight = countRight; }
-    public char result() {
-        switch (Integer.compare(this.countLeft, this.countRight)) {
-            case 1: return 'L';
-            case -1: return 'R';
-            default: return '=';
-        }
-    }
+    public void addLeft(int countLeft) { }
+    public void addRight(int countRight) { }
+    public char result() { return 'a'; }
 }
